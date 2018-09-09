40395042345d31f76204734d03e8a5d8add18df2
ulkoenig <uk.koenig@t-online.de>
2018-09-09 19:44:17 +0200

    fix Eclipse

--- src/ubuntu/install/eclipse.sh
+++ src/ubuntu/install/eclipse.sh
@@ -3,8 +3,10 @@
 set -e
 
 echo "Install Eclipse"
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
 wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/photon/M2/eclipse-dsl-photon-M2-linux-gtk-x86_64.tar.gz -O eclipse.tag.gz
 mkdir /usr/local/etc/eclipse
-
 tar -xzf eclipse.tag.gz -C /usr/local/etc/eclipse/
-ln -s /usr/local/bin /usr/local/etc/eclipse
+ln -s /usr/local/etc/eclipse/eclipse/eclipse /usr/local/bin/eclipse

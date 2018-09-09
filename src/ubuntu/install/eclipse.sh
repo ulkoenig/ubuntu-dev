1b021a079f993fd9847473c631e21e015a7f2f25
ulkoenig <uk.koenig@t-online.de>
2018-09-09 19:23:12 +0200

    add Eclipse

--- src/ubuntu/install/eclipse.sh
+++ src/ubuntu/install/eclipse.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "Install Eclipse"
 wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/photon/M2/eclipse-dsl-photon-M2-linux-gtk-x86_64.tar.gz -O eclipse.tag.gz
-mkdir /usr/local/etc
 mkdir /usr/local/etc/eclipse
 
 tar -xzf eclipse.tag.gz -C /usr/local/etc/eclipse/

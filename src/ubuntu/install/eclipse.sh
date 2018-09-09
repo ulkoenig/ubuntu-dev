e205da72a4c217b3c8b35e3662307bea6a48ae20
ulkoenig <uk.koenig@t-online.de>
2018-09-09 20:50:15 +0200

    fix Eclipse

--- src/ubuntu/install/eclipse.sh
+++ src/ubuntu/install/eclipse.sh
@@ -6,7 +6,7 @@ echo "Install Eclipse"
 apt-get update
 apt-get install -y default-jdk
 apt-get clean -y
-wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/photon/R/eclipse-java-photon-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+wget http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/photon/M2/eclipse-dsl-photon-M2-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
 mkdir /usr/local/etc/eclipse
 tar -xzf eclipse.tar.gz -C /usr/local/etc/eclipse/
 ln -s /usr/local/etc/eclipse/eclipse/eclipse /usr/local/bin/eclipse

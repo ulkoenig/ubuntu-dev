2b3b11e2c7cf45795478497c8db688bcd11f3360
ulkoenig <uk.koenig@t-online.de>
2019-02-24 18:03:34 +0100

    fix download link for IntelliJ

--- src/ubuntu/install/intellij.sh
+++ src/ubuntu/install/intellij.sh
@@ -9,6 +9,7 @@
 set -e
 
 echo "Install IntelliJ"
-wget https://www.jetbrains.com/idea/download/download-thanks.html?platform=linuxWithoutJDK&code=IIC -O intellij.tar.gz
+
+wget https://download.jetbrains.com/idea/ideaIC-2018.3.4.tar.gz -O intellij.tar.gz
 tar -xzf intellij.tar.gz -C /usr/local/etc/
 # ln -s /usr/local/etc/intellij/intellij /usr/local/bin/intellij

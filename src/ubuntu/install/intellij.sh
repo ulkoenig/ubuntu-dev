563ea22555eeed68fdf0a6f29aee82bec6ccf560
ulkoenig <uk.koenig@t-online.de>
2019-02-24 19:40:24 +0100

    add menu item for intelliJ

--- src/ubuntu/install/intellij.sh
+++ src/ubuntu/install/intellij.sh
@@ -7,3 +7,20 @@ echo "Install IntelliJ"
 wget https://download.jetbrains.com/idea/ideaIC-2018.3.4.tar.gz -O intellij.tar.gz
 tar -xzf intellij.tar.gz -C /usr/local/etc/
 ln -s /usr/local/etc/idea*/bin/idea.sh /usr/local/bin/idea.sh
+
+# Add item to xfce application menu
+cat > /usr/share/applications/intellij.desktop << EOF
+[Desktop Entry]
+Name=IntelliJ
+GenericName=IntelliJ IDE
+GenericName[de]=IntelliJ IDE
+GenericName[en]=IntelliJ IDE
+Comment=Developer IDE
+Exec=idea.sh
+Icon=/usr/local/etc/idea-IC-183.5429.30/bin/idea.png
+Terminal=false
+Type=Application
+StartupNotify=false
+Categories=Development
+OnlyShowIn=GNOME;XFCE;
+EOF

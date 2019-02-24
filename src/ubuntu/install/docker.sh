3bb1e721080edc04b98264da3535d4be05433934
ulkoenig <uk.koenig@t-online.de>
2019-02-24 22:18:33 +0100

    add docker

--- src/ubuntu/install/docker.sh
+++ src/ubuntu/install/docker.sh
@@ -9,5 +9,7 @@ apt-get clean -y
 
 wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/containerd.io_1.2.2-3_amd64.deb -O containerd.deb 
 dpkg -i containerd.deb
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce-cli_18.09.2~3-0~ubuntu-xenial_amd64.deb -O docker-cli.deb
+dpkg -i docker-cli.deb
 wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_18.09.2~3-0~ubuntu-xenial_amd64.deb -O docker.deb
 dpkg -i docker.deb
